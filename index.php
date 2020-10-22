53e91017f8874adea2706fb90770b2b0a4047fd8
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-10-22 17:03:58 +0200

    prove

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+  class cliente {
+    public $nome;
+    public $cognome;
+    public $eta;
+    private $tel;
+    public $email;
+    public $citta;
+  }
+
+  $nuovoCliente = new cliente();
+  $nuovoCliente -> nome = "Gabriele"
+  $nuovoCliente -> cognome = "Musumeci"
+  $nuovoCliente -> tel = "123456789";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>OOP</title>
+  </head>
+
+  <body>
+    <h1>Prova.</h1>
+    <h2></h2>
+  </body>
+</html>
