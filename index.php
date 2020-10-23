ed19b470dcddd85dcd654009b7ebf7bedbced44a
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-10-23 16:21:02 +0200

    Prove

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <?php
 
-  class cliente {
+  // classe del vicinato
+  class Vicini {
     public $nome;
     public $cognome;
-    public $eta;
-    private $tel;
-    public $email;
-    public $citta;
+    public $anzianita;
+    public $scontoCondominio = "no";
+
+    // funzione di sconto sul condominio per i vecchi condomini
+    public function sconto()
+    {
+      if($anzianita > 20){
+        $this->scontoCondominio = "Sconto 20%";
+      }
+      elseif ($anzianita > 30){
+        $this->scontoCondominio = "Sconto 50%";
+      }
+      else{
+        $scontoCondominio = "No Sconto";
+      }
+    }
+
+
+
   }
 
-  $nuovoCliente = new cliente();
-  $nuovoCliente -> nome = "Gabriele"
-  $nuovoCliente -> cognome = "Musumeci"
-  $nuovoCliente -> tel = "123456789";
+
+  // Utenti del Condominio
+  $int1 = new Vicini("Gabriele", "Musumeci", 21, sconto());
+  $int2 = new Vicini("Gino", "Alibaba", 30, sconto());
+  $int3 = new Vicini("Maurizio", "Bau", 39, sconto());
+  $int4 = new Vicini("Alfonzo", "Pascucci", 5, sconto());
+
+  $condominio = [$int1, $int2, $int3, $int4];
 
 ?>
 
@@ -21,10 +41,34 @@
   <head>
     <meta charset="utf-8">
     <title>OOP</title>
+    <link rel="stylesheet" href="style.css">
   </head>
-
   <body>
-    <h1>Prova.</h1>
-    <h2></h2>
+    <table>
+      <!-- intestazione tabella -->
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Cognome</th>
+          <th>Anzianita'</th>
+          <th>Sconto</th>
+        </tr>
+      </thead>
+      <!--// intestazione tabella -->
+
+      <!-- corpo tabella -->
+      <tbody>
+        <?php foreach ($condominio as $condomine): ?>
+          <tr>
+            <td><?php echo $condomine->name; ?></td>
+            <td><?php echo $condomine->cognome; ?></td>
+            <td><?php echo $condomine->anzianita; ?></td>
+            <td><?php echo $condomine->scontoCondominio; ?></td>
+          </tr>
+        <?php endforeach ?>
+
+      </tbody>
+    <!--// corpo tabella -->
+    </table>
   </body>
 </html>
